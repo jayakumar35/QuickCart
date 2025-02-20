2a26508f4cd4fcbc4e085f0d1030d7b3b97844a9
jayakumar35 <jayakumar53c@gmail.com>
2025-02-20 13:22:02 +0530

    modify commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,11 @@
 import { serve } from "inngest/next";
-import { inngest, sycnUserUpdate, syncUserCreation } from "@/config/inngest";
-
+import { inngest, sycnUserUpdate, syncUserCreation, syncUserDeletion } from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
         syncUserCreation,
         sycnUserUpdate,
-        syncUserCreation
+        syncUserDeletion
     ],
 });
